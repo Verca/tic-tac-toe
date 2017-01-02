a61e6b6f5dfde33e4a7080a577681bfe004b46e4
Verca Crkvova <verca@spotify.com>
2017-01-02 16:43:10 +0100

    webpack page reload work now

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,24 @@ var glob = require('glob');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './js/applicationSettings/app.jsx', './assets/globalStyles/index.less'],
+  entry: [
+    'babel-polyfill',
+    './js/applicationSettings/app.jsx',
+    'webpack-dev-server/client?http://localhost:8080',
+    './assets/globalStyles/index.less'
+  ],
   output: {
     path: './dist',
     filename: 'bundle.js',
   },
   plugins: [
     // new webpack.NoErrorsPlugin(),
-    new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /a\.js|node_modules/,
-      // add errors to webpack instead of warnings
-      failOnError: false
-    }),
+    // new CircularDependencyPlugin({
+    //   // exclude detection of files based on a RegExp
+    //   exclude: /a\.js|node_modules/,
+    //   // add errors to webpack instead of warnings
+    //   failOnError: false
+    // }),
     // extract CSS into separate file
     new ExtractTextPlugin('app.bundle.global.css')
   ],
@@ -28,33 +33,21 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        include: [path.resolve(__dirname, 'js', 'modules')],
-        exclude: /node_modules/,
-        loaders: ['babel-loader', 'eslint-loader']
-      },
-    ],
     loaders: [
       {
         test: /\.jsx?$/,
+        include: [path.resolve(__dirname, 'js')],
         exclude: /node_modules/,
-        loader: 'babel'
+        loaders: ['babel', 'eslint-loader']
       },
-      // {
-      //   test: /\.jsx?$/,
-      //   exclude: /node_modules/,
-      //   loaders: ['babel-loader', 'eslint-loader']
-      // },
       {
         test: /\.less$/,
-        include: [path.resolve(__dirname, 'assets'), path.resolve(__dirname, 'dist')],
+        include: [path.resolve(__dirname, 'assets')],
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
       },
       {
         test: /\.less$/,
-        include: [path.resolve(__dirname, 'js', 'modules')],
+        include: [path.resolve(__dirname, 'js')],
         loader: 'style-loader!css-loader?modules&importLoaders=2&sourceMap&localIdentName=[path][name]---[local]---[hash:base64:5]!less-loader'
       },
       { test: /\.css$/, loader: 'style-loader!css-loader?modules' },
